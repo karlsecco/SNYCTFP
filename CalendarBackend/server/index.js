2432b672644dd662a0cdeeba26841212666a42e1
karlsecco <karlsecco@gmail.com>
2018-06-29 07:06:54 -0700

    mostly completed, untested, WIP--adding error troubleshooting comments

--- CalendarBackend/server/index.js
+++ CalendarBackend/server/index.js
@@ -4,8 +4,22 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 // const MONGODB_URI = process.env.MONGODB_URI;
+
+/*
+I have been unable to solve or successfully research a solution
+to the following error when running nodemon in terminal:
+
+"UnhandledPromiseRejectionWarning: Error: URL malformed, cannot be parsed".
+
+Unfortunately, troubleshooting this has set me back a significant portion
+of the allotted time in order to be able to test and complete the assignment.
+The only work-around I know is to release my database information publicly--
+a temporary solution I was hoping to avoid if even for a toy project.
+*/
+
 const MONGODB_URI =
   'mongodb://heroku_cn3rvrgb:2fhnrl9nkp1a1blrbh80t9jbg1@ds123151.mlab.com:23151/heroku_cn3rvrgb';
+
 const PORT = 5000;
 
 const { eventsRouter } = require('./routers');
